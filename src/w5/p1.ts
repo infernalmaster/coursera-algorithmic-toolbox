d64191b5f0188f31dbefd316afb255c1bd0379ef
Rostyslav Diachok <infernalmaster1@gmail.com>
2016-07-31 15:41:16 +0300

    simplify w5p1

--- src/w5/p1.ts
+++ src/w5/p1.ts
@@ -26,38 +26,34 @@ export default function calculator(input: number): number[] {
 export function calculatorMemoryOptimized(input: number): number[] {
   const steps = [0, 1, 2, 2];
   const ascSort = (x, y) => x - y;
-  const filterNonEmpty = (val) => !!val;
 
   for (let i = 4; i <= input; i++) {
-    let a = i % 3 === 0 ? steps[i / 3] : null;
-    let b = i % 2 === 0 ? steps[i / 2] : null;
+    let a = i % 3 === 0 ? steps[i / 3] : Infinity;
+    let b = i % 2 === 0 ? steps[i / 2] : Infinity;
     let c = steps[i - 1];
 
-    let min = [a, b, c].filter(filterNonEmpty).sort(ascSort)[0]
+    let min = [a, b, c].sort(ascSort)[0]
 
     steps[i] = min + 1;
   }
 
-  // now use backtrace to find sequence of numbers
-  const sequenceOfNumbers = [input];
-  let currentNumber = input;
-  while (currentNumber > 1) {
-    const numberOfSteps = steps[currentNumber] - 1;
-    let prevNumber;
-    for (let testNumber = 0; testNumber < steps.length; testNumber++) {
-      const hasRightNumberOfSteps = steps[testNumber] === numberOfSteps;
-      const canBeTranformedToNextNumber = currentNumber / 3 === testNumber ||
-                                          currentNumber / 2 === testNumber ||
-                                          currentNumber - 1 === testNumber;
-      if (hasRightNumberOfSteps && canBeTranformedToNextNumber) {
-        prevNumber = testNumber;
-        break;
-      }
-    }
 
-    sequenceOfNumbers.unshift(prevNumber);
-    currentNumber = prevNumber;
+  // now use backtrace to find sequence of numbers
+  let numb = input;
+  const sequenceOfNumbers = [];
+  const ascSortBySteps = (x, y) => x.steps - y.steps;
+  while (numb > 0) {
+    sequenceOfNumbers.unshift(numb);
+
+    let a = numb % 3 === 0 ? { steps: steps[numb / 3], number: numb / 3 } : { steps: Infinity, number: 0 };
+    let b = numb % 2 === 0 ? { steps: steps[numb / 2], number: numb / 2 } : { steps: Infinity, number: 0 };
+    let c = { steps: steps[numb - 1], number: numb - 1 };
+
+    let min = [a, b, c].sort(ascSortBySteps)[0];
+
+    numb = min.number;
   }
+
 
   return sequenceOfNumbers;
 }
